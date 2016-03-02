d75185669c442c2636eb1e56cb02ee93ac2ea771
Stuart Pook <stuart12@users.noreply.github.com>
2016-03-02 22:01:41 +0100

    use Lensfun & lensfunpy to remove distortion

--- rotate-jpeg.py
+++ rotate-jpeg.py
@@ -23,37 +23,38 @@ import Image
 import string
 import optparse
 import pyexiv2
+import collections
+import piexif
+import cv2
+import lensfunpy
+
+import gi.repository
+gi.require_version('GExiv2', '0.10')
+from gi.repository import GExiv2
+
+def error(*message):
+	print(*message, file=sys.stderr)
+	sys.exit(6)
 
 def update_size(dest, image):
 	# http://stackoverflow.com/questions/400788/resize-image-in-python-without-losing-exif-data
 	dest["Exif.Photo.PixelXDimension"] = image.size[0]
 	dest["Exif.Photo.PixelYDimension"] = image.size[1]
 
+def read_exif_data(image, source_path, options):
+	if pyexiv2.version_info < (0, 3, 2):
+		error("pyexiv2 too old")
+	source = pyexiv2.ImageMetadata(source_path)
+	source.read()
+	return source
+
 def copy_exif_data(image, source_path, dest_path, options):
-	if pyexiv2.version_info >= (0, 3, 2):
-		source = pyexiv2.ImageMetadata(source_path)
-		source.read()
-		dest = pyexiv2.ImageMetadata(dest_path)
-		dest.read()
-		source.copy(dest)
-		update_size(dest, image)
-		dest.write()
-	else:
-		source = pyexiv2.Image(source_path)
-		source.readMetadata()
-		dest = pyexiv2.Image(dest_path)
-		dest.readMetadata()
-		for k in source.exifKeys():
-			v = source[k]
-			try:
-				dest[k] = source[k]
-			except ValueError as e:
-				if options.verbose:
-					print("skipping Exif tag", k, v, e.args[0])
-				pass
-		update_size(dest, image)
-	#	source.copyMetadataTo(dest)
-		dest.writeMetadata()
+	source = read_exif_data(image, source_path, options)
+	dest = pyexiv2.ImageMetadata(dest_path)
+	dest.read()
+	source.copy(dest)
+	update_size(dest, image)
+	dest.write()
 
 def shrink(inputfile, options):
 	im = Image.open(inputfile)
@@ -73,8 +74,87 @@ def shrink(inputfile, options):
 		im.save(options.output, "JPEG", quality=options.quality)
 		copy_exif_data(im, inputfile, options.output, options)
 		if os.path.getsize(options.output) > os.path.getsize(inputfile) * 1.2:
-		    os.remove(options.output)
-		    os.symlink(inputfile, options.output)
+			os.remove(options.output)
+			os.symlink(inputfile, options.output)
+
+def get_required_exif_data(exif):
+	# https://git.gnome.org/browse/gexiv2/tree/GExiv2.py
+	cam_maker = exif.get('Exif.Image.Make')
+	if cam_maker is None:
+		return None
+	camera_model = exif.get('Exif.Image.Model')
+	if camera_model is None:
+		return None
+
+	focallength = exif.get_exif_tag_rational('Exif.Photo.FocalLength')
+	if focallength is None:
+		return None
+
+	aperture = exif.get_exif_tag_rational('Exif.Photo.ApertureValue')
+	if aperture is None:
+		return None
+
+	distance = exif.get_exif_tag_rational('Exif.Photo.SubjectDistance')
+	if distance is None:
+		return None
+
+	PhotoData = collections.namedtuple('PhotoData', ['camera_maker', 'camera_model', 'focal_length', 'aperture', 'subject_distance'])
+	return PhotoData(
+		camera_maker=cam_maker,#.encode('ascii','ignore'),
+		camera_model=camera_model,#.encode('ascii','ignore'),
+		focal_length=focallength,
+		aperture=aperture,
+		subject_distance=distance
+		)
+
+def get_size(iw, ih, width, height):
+	if width >= iw and height >= ih:
+		return None
+	rw = width / float(iw)
+	rh = height / float(ih)
+	if rw > rh:
+		return (int(iw * rh), height)
+	return (width, int(ih * rw))
+
+def undistort(inputfile, options):
+	exif = GExiv2.Metadata(inputfile)
+	required_info = get_required_exif_data(exif)
+	if required_info is None:
+		return shrink(inputfile, options)
+
+	db = lensfunpy.Database()
+	cam = db.find_cameras(required_info.camera_maker, required_info.camera_model)[0]
+	lens = db.find_lenses(cam)[0]
+
+	im = cv2.imread(inputfile)
+	ih, iw = im.shape[0], im.shape[1]
+
+	width = options.width if options.width else iw
+	height = options.height if options.height else ih
+	if ih > iw:
+		width, height = height, width
+
+	mod = lensfunpy.Modifier(lens, cam.crop_factor, iw, ih)
+	mod.initialize(required_info.focal_length, required_info.aperture, required_info.subject_distance)
+
+	undistCoords = mod.apply_geometry_distortion()
+	imUndistorted = cv2.remap(im, undistCoords, None, cv2.INTER_NEAREST)
+	#http://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html
+	sz = get_size(iw, ih, width, height)
+	if sz:
+		shrunk = cv2.resize(imUndistorted, sz, interpolation=cv2.INTER_AREA)
+		#print("ratio is (%d %d) != (%d %d)" % (shrunk.shape[1], shrunk.shape[0], iw, ih))
+		if shrunk.shape[0] != height and shrunk.shape[1] != height and shrunk.shape[0] != width and  shrunk.shape[1] != width:
+			error("new size is wrong (%d %d) != (%d %d)" % (shrunk.shape[1], shrunk.shape[0], iw, ih))
+		if abs(shrunk.shape[0] / float(shrunk.shape[1]) - im.shape[0] / float(im.shape[1])) > options.ratio_change:
+			error("ratio changed (%d %d) != (%d %d)" % (shrunk.shape[1], shrunk.shape[0], im.shape[1], im.shape[0]))
+	else:
+		shrunk = imUndistorted
+	cv2.imwrite(options.output, shrunk, (cv2.IMWRITE_JPEG_QUALITY, options.quality))
+
+	exif['Exif.Photo.PixelXDimension'] = str(width)
+	exif['Exif.Photo.PixelYDimension'] = str(height)
+	exif.save_file(options.output)
 
 def main():
 	parser = optparse.OptionParser(usage="usage: %prog [--help] [options] inputfile")
@@ -83,9 +163,10 @@ def main():
 	parser.add_option("--resizing_pp3", metavar="DUMMY", help="ignored [%default]")
 	parser.add_option("--no_dimensions_symlink", action="store_true", help="don't make any symlinks even if the dimensions seem ok")
 	parser.add_option("-m", "--size_margin", type='float', default=1.1, help="margin for files almost the same size [%default]")
+	parser.add_option("--ratio_change", type='float', default=0.001, help="maximum accepted ratio change after resize")
 	parser.add_option("-w", "--width", type='int', default=None, help="output width [%default]")
 	parser.add_option("--height", type='int', default=None, help="output height [%default]")
-	parser.add_option("-q", "--quality", type='int', default=40, help="output JPEG quality [%default]")
+	parser.add_option("-q", "--quality", type='int', default=20, help="output JPEG quality [%default]")
 	(options, args) = parser.parse_args()
 	if len(args) != 1:
 		parser.error("must supply 1 argument (found %d %s)" % (len(args), args))
@@ -93,7 +174,7 @@ def main():
 	if not options.output:
 		parser.error("--output option is compulsory")
 		
-	shrink(inputfile, options)
+	undistort(inputfile, options)
 	return 0
 
 if __name__ == "__main__":
