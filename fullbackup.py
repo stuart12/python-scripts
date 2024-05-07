965373f4e0a4488e9ee2b78811806b1bb5249bd2
Stuart Pook <stuart12@users.noreply.github.com>
2024-05-07 23:25:24 +0200

    fullbackup March 2024

--- fullbackup.py
+++ fullbackup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3 -P
-# mk-archive Copyright (c) 2013 Stuart Pook (http://www.pook.it/)
+# fullbackup Copyright (c) 2024 Stuart Pook (http://www.pook.it/)
 # make a squashfs on luks image of all my files to be archived
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -39,6 +39,7 @@ import math
 import pwd
 import argparse
 import logging
+import collections
 
 class MyError(Exception):
     """Base class for exceptions in this module."""
@@ -62,11 +63,11 @@ def print_command(command, cwd=None, stdin=None):
 
 def call(command,  **kwargs):
     logging.info("running: %s",  command)
-    subprocess.check_call(args=command, **kwargs)
+    subprocess.run(args=command, text=True, check=True, **kwargs)
  
 def sudo(command,  **kwargs):
     cmd = ['sudo'] + command
-    call(cmd,  **kwargs)
+    call(cmd, **kwargs)
 
 def output(command, stdin=None):
     print_command(command, stdin=stdin)
@@ -78,56 +79,31 @@ def output(command, stdin=None):
 
 def output_sudo(command, stdin=None):
     return output(["sudo"] + command, stdin=stdin)
-
-    os.stat(mount_point)
-    if os.path.ismount(mount_point):
-        call_sudo(options, [command, mount_point])
-
-    if os.path.exists(f):
-        raise MyError(f + " must not exist")
-
-    try:
-        os.remove(name)
-    except IOError as e:
-        if e.errno != errno.ENOENT:
-            raise
-    image = open(name, "w")
-    image.seek(size - 1)
-    image.write("\0")
-    image.flush()
-    image.seek(0)
-    assert os.fstat(image.fileno()).st_size == size
-    return image
-
-    image_size = int(output_sudo(["blockdev", "--getsize", loop]))
-    crypt_size = int(output_sudo(["blockdev", "--getsize", crypt_device]))
-    return image_size - crypt_size
-
-    call_sudo(options, ["cp", squash_file, crypt_device])
-
-    with make_sparse_file(image_filename, size) as image:
-        loop =  output_sudo(["losetup", "--show", "-f", image_filename])
-        try:
-            call_sudo(options, ["sudo", "cryptsetup", "luksFormat", loop, "-"], stdin=passwords[0])
-            for f in  passwords[1:]:
-                call_sudo(options, ["cryptsetup", "luksAddKey", loop, "--key-file", passwords[0], f])
-            call_sudo(options, ["cryptsetup", "luksOpen", "--key-file", passwords[-1], loop, options.crypt_device])
-            try:
-                crypt_device = os.path.join("/dev/mapper", options.crypt_device)
-                return func(loop, crypt_device, arg, options)
-            finally:
-                call_sudo(options, ["cryptsetup", "remove", options.crypt_device])
-        finally:
-            call_sudo(options, ["losetup", "-d", loop])
-
     
-def luks(usb,  passwords,  label,  root_device):
-    password = os.path.join(passwords, 'password0')
+def backup(directories,  device,  home,  mount_point,  mkfs_options,  label,  dryrun):
+    logging.info('backup %s in %s to %s', directories,  home,  device)
+    if dryrun: return
+    call(['mksquashfs'] + directories + [device, '-noappend'] + mkfs_options,  cwd=home)
+    sudo(['mount',  '-o',  f"X-mount.idmap=b:0:{os.getuid()}:1",  os.path.join('/dev/mapper',  label), mount_point])
+
+PasswordFile = collections.namedtuple("PasswordFile", "fn size")
+
+def get_password(passwords, name):
+    fn = os.path.join(passwords, name)
+    with open( os.path.join(passwords, fn)) as pw:
+        return PasswordFile(fn, str(len(pw.readline().strip())))
+    
+def luks(passfn, usb,  passwords,  label,  root_device):
+    luks_pw = get_password(passwords, 'luks')
+    passwd_pw = get_password(passwords, passfn)
+
     with open('/dev/null',  'w') as devnull:
-        sudo(['cryptsetup',  'luksDump',  '--dump-volume-key',  '--batch-mode',  '--key-file',  password,  root_device], stdout=devnull)
-    sudo(['cryptsetup',  'luksFormat',  '--label', label,'--type',  'luks2', '--integrity',  'hmac-sha256',  usb, password]) 
-    sudo(['cryptsetup',  'luksOpen', '--key-file', password,  usb, label])
+        sudo(['cryptsetup',  'luksDump', '--dump-volume-key', '--batch-mode', '--key-file', luks_pw.fn, '--keyfile-size', luks_pw.size, root_device], stdout=devnull)
+    sudo(['cryptsetup', 'luksFormat', '--label', label,'--type', 'luks2', '--key-file', luks_pw.fn, '--keyfile-size', luks_pw.size, usb]) 
+    sudo(['cryptsetup', 'luksAddKey', '--key-file', luks_pw.fn, '--keyfile-size', luks_pw.size, '--new-keyfile', passwd_pw.fn, '--new-keyfile-size', passwd_pw.size, usb])
+    sudo(['cryptsetup', 'luksOpen', '--key-file',  passwd_pw.fn, '--keyfile-size', passwd_pw.size, usb, label])
     sudo(['chown',  str(os.getuid()),  os.path.join('/dev/mapper',  label)])
+
     
 def run_scripts(scripts,  directory,  home):
     return [os.path.relpath(subprocess.check_output([os.path.join(directory,  s),  '--cache_filename'],  text=True).strip(),  start=home) for s in scripts]
@@ -136,14 +112,16 @@ def mkdir(path,  dryrun):
     logging.debug("mkdir %s",  path)
     os.mkdir(path, mode=0o700)
     
-def decrypt_passwords(home,  subdir,  tmpdir,  dryrun):
+def decrypt_passwords(passfn, home,  subdir,  tmpdir,  dryrun):
     start = os.path.join(home,  subdir)
     logging.info("tmpdir %s",  tmpdir)
     out = os.path.join(tmpdir,  subdir)
     mkdir(path=out,  dryrun=dryrun)
+    count = 0
+    logging.info("decrypt passwords %s -> %s", start,  out)
     for root,  dirs,  files in os.walk(start):
         target = os.path.join(out, os.path.relpath(root,  start=start))
-        logging.info("root %s start %s -> current %s",  root,  start,  target)
+        logging.debug("root %s start %s -> current %s",  root,  start,  target)
         skip = set()
         for index,  name in enumerate(dirs):
             if name == '.git':
@@ -158,9 +136,12 @@ def decrypt_passwords(home,  subdir,  tmpdir,  dryrun):
             if name.endswith(suffix):
                 encrypted = os.path.join(root,  name)
                 cleartext = os.path.join(target,  name[:-len(suffix)] + '.txt')
-                command = ['gpg', '--decrypt',  '--output',  cleartext,   '--quiet',  encrypted]
+                command = ['gpg', '--passphrase-fd=0', '--pinentry-mode', 'loopback', '--batch', '--decrypt', '--output', cleartext, '--quiet', encrypted]
                 logging.debug("running: %s",  command)
-                subprocess.check_call(args=command)
+                with open(passfn) as fp:
+                    subprocess.run(args=command, check=True, stdin=fp)
+                count += 1
+    logging.info("decrypted %d passwords %s -> %s", count,  start,  out)
     return out
     
 def download_email():
@@ -170,23 +151,23 @@ def main():
     parser = argparse.ArgumentParser(description="do full backup",  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.set_defaults(loglevel='info')
     parser.set_defaults(dryrun=False)
-    parser.add_argument("-v", "--verbose", dest='loglevel', action="store_const", const='debug', help="debug loglevel")
+    parser.add_argument("-v", "--debug", "--verbose", dest='loglevel', action="store_const", const='debug', help="debug loglevel")
     parser.add_argument("-n", "--dryrun", action="store_true", help="dryrun")
     parser.add_argument("--luks", action="store_true", help="setup luks")
- #   parser.add_argument("--width", type=int, help="width")
+    parser.add_argument("--pass", dest="passfn", default="pass", help="file containing password for pass password store")
     parser.add_argument("--passwords", default="/tmp/passwords", help="directory of passwords")
     parser.add_argument("--usb", default="/dev/disk/by-id/usb-_USB_DISK_3.2_6198CC39-0:0-part1", help="USB device")
     parser.add_argument("--root_device", default="/dev/disk/by-diskseq/1-part6", help="USB device")
-    parser.add_argument("--label", default="backup-2023", help="luks label")
-    parser.add_argument("--home", default=os.path.expanduser('~') ,  help="root directory")
-    parser.add_argument('--mkfs_options', default=['-all-root',  '-root-mode',  '-keep-as-directory',  '-no-recovery',  '-no-strip', '-root-mode',  '0700'],  help='options to mksquashfs')
+    parser.add_argument("--label", default="backup-2024", help="luks label")
+    parser.add_argument("--home", default=os.path.expanduser('~'), help="root directory")
+    parser.add_argument('--mkfs_options', default=['-all-root', '-keep-as-directory', '-no-strip', '-root-mode',  '0700'],  help='options to mksquashfs')
     parser.add_argument('--script_directory', default=os.path.dirname(sys.argv[0]),  help='directory of scripts')
-    parser.add_argument('--scripts', default=['ring',  'calendar.py'],  help='scripts to run')
+    parser.add_argument('--scripts', default=['ring', 'calendar'],  help='scripts to run')
     parser.add_argument('--mount_point', default='/mnt',  help='where to mount')
     parser.add_argument('--password_store', default='.password-store',  help='directory of passwords')
     parser.add_argument('--extras',  default=['archive'],  help='extra directories to backup')
 
-    parser.add_argument('directories', nargs='*', default=['Syncthing/stuart',  'photos',  'Books'],  help='directories to backup')
+    parser.add_argument('directories', nargs='*', default=['Syncthing/stuart', 'archive', 'photos',  'Books'],  help='directories to backup')
     options = parser.parse_args()
 
     numeric_level = getattr(logging, options.loglevel.upper(), None)
@@ -196,12 +177,14 @@ def main():
 
     mapped =  os.path.join('/dev/mapper',  options.label)
     if options.luks:
-        luks(passwords=options.passwords,  usb=options.usb, label=options.label,  root_device=options.root_device)
+        luks(passfn=options.passfn, passwords=options.passwords,  usb=options.usb, label=options.label,  root_device=options.root_device)
     else:
         caches = run_scripts(scripts=options.scripts, home=options.home, directory=options.script_directory)
         tmpdir = tempfile.mkdtemp(prefix='full-backup')
         try:
-            passwds = decrypt_passwords(home=options.home,  subdir=options.password_store, tmpdir=tmpdir,  dryrun=options.dryrun)
+            passwds = decrypt_passwords(
+                    passfn=os.path.join(options.passwords, options.passfn), home=options.home,
+                    subdir=options.password_store, tmpdir=tmpdir,  dryrun=options.dryrun)
             backup(directories=caches + options.directories + [passwds],  device=mapped,  home=options.home,
                 mount_point=options.mount_point, label=options.label,  dryrun=options.dryrun,
                 mkfs_options=options.mkfs_options)
